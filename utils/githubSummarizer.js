318d68ab021fe4f559f6610f91fd68068ce40c06
hreid-cloud <64999256+hreid-cloud@users.noreply.github.com>
2024-12-10 14:28:47 +1300

    github summarizer final works!!!

--- utils/githubSummarizer.js
+++ utils/githubSummarizer.js
@@ -1,10 +1,13 @@
 import { ChatOpenAI } from "@langchain/openai";
-import { PromptTemplate } from "@langchain/prompts";
-import { RunnableSequence } from "@langchain/schema/runnable";
-import { StructuredOutputParser } from "@langchain/output_parsers";
+import { ChatPromptTemplate} from "@langchain/core/prompts";
+import { StructuredOutputParser } from "@langchain/core/output_parsers";
 import { z } from "zod";
 
 export async function summarizeGithubRepo(readmeContent) {
+  if (!readmeContent) {
+    throw new Error("README content is required but was not provided");
+  }
+
   // Create parser for structured output
   const parser = StructuredOutputParser.fromZodSchema(
     z.object({
@@ -13,35 +16,32 @@ export async function summarizeGithubRepo(readmeContent) {
     })
   );
 
+  // Format instructions for the model
+  const formatInstructions = parser.getFormatInstructions();
+
   // Create the summarization chain with updated prompt
-  const prompt = PromptTemplate.fromTemplate(
-    `Summarize this github repository:
+  const prompt = ChatPromptTemplate.fromTemplate(
+    `Summarize this github repository from this README file content:
     
-    {readme}
+    {readmeContent}
     
-    ${parser.getFormatInstructions()}
+    {format_instructions}
     
     Provide:
     1. A clear and concise summary that explains the main purpose of the repository, key features, and notable technical details
     2. A list of 3-5 interesting technical facts about the repository`
   );
 
-  const model = new ChatOpenAI({
+  const model = new ChatOpenAI({ 
     temperature: 0.0,
-    modelName: "gpt-3.5-turbo",
+    openAIApiKey: process.env.OPEN_AI_KEY 
   });
 
-  const chain = RunnableSequence.from([
-    {
-      readme: (input) => input.readme,
-    },
-    prompt,
-    model,
-    parser,
-  ]);
+  const chain = prompt.pipe(model).pipe(parser);
 
   // Run the chain
   return await chain.invoke({
-    readme: readmeContent,
+    readmeContent: readmeContent,
+    format_instructions: formatInstructions,
   });
 } 
